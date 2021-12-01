13d6ee7ec1cff61aab9b7e78a2dbb3ac121dcd58
Peter Blanchfield <zygomy@gmail.com>
2021-12-01 16:45:20 +0000

    Added a bad test on purpose

--- PersonUnitTest/Person.cs
+++ PersonUnitTest/Person.cs
@@ -3,8 +3,24 @@ namespace PersonUnitTest
 {
     internal class Person
     {
+        string firstName;
+        string lastName;
+        public string FirstName {
+            get { 
+                return firstName; 
+            }
+        }
+
+        public string LastName { 
+            get { 
+                return lastName; 
+            } 
+        }
+
         public Person(string v1, string v2)
         {
+            firstName = v1;
+            lastName = v2;
         }
     }
 }
--- PersonUnitTest/UnitTest1.cs
+++ PersonUnitTest/UnitTest1.cs
@@ -1,30 +1,45 @@
 using NUnit.Framework;
-using System.Collections;
+using System.Collections.Generic;
 using System;
 namespace PersonUnitTest
 {
     public class Tests
     {
         Person p;
-        String s;
+        List<Person> people;
         [SetUp]
         public void Setup()
         {
             //From our project description a person will have a first name and last name
             p = new Person("Fred", "Bloggs");
+            people = new List<Person>();
         }
 
         [Test]
         public void Test1()
         {
             
-            Assert.Pass();
+            Assert.AreEqual("Fred",p.FirstName);
         }
+        [Test]
+        public void Test2()
+        {
 
+            Assert.AreEqual("Bloggs", p.LastName);
+        }
+        //The following is a bad test
+        [Test]
+        public void Test3()
+        {
+            people.Add(p);
+            Assert.AreNotEqual(people[0].FirstName, "Fred");
+            Assert.AreEqual(people[0], p);
+        }
+        //In tear down we ought to make sure that everything is closed 
         [TearDown]
         public void TearDown()
         {
-
+            people.Clear();
         }
     }
 }
