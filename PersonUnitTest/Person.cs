7ad63f14d75786a16e545cfd4e7ca1299663fa73
Peter Blanchfield <zygomy@gmail.com>
2021-12-01 15:23:40 +0000

    Initial setup complete

---
+++ PersonUnitTest/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace PersonUnitTest
+{
+    internal class Person
+    {
+        public Person(string v1, string v2)
+        {
+        }
+    }
+}
--- PersonUnitTest/UnitTest1.cs
+++ PersonUnitTest/UnitTest1.cs
@@ -1,18 +1,30 @@
 using NUnit.Framework;
-
+using System.Collections;
+using System;
 namespace PersonUnitTest
 {
     public class Tests
     {
+        Person p;
+        String s;
         [SetUp]
         public void Setup()
         {
+            //From our project description a person will have a first name and last name
+            p = new Person("Fred", "Bloggs");
         }
 
         [Test]
         public void Test1()
         {
+            
             Assert.Pass();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
         }
     }
 }
